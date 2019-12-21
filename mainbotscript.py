977b1c5fb273589766bac5590d66648f8017b242
tel3port <carn3lian@gmail.com>
2019-12-21 17:52:05 +0300

    first commit

--- mainbotscript.py
+++ mainbotscript.py
@@ -12,21 +12,26 @@ class InstagramBot:
         self.password = password
         self.driver = webdriver.Chrome("./chromedriver")
         self.login()
+        self. base_url = "https://www.instagram.com"
 
     def login(self):
         options = webdriver.ChromeOptions()
         options.add_argument("start-maximized")
         options.add_experimental_option("excludeSwitches", ["enable-automation"])
         options.add_experimental_option('useAutomationExtension', False)
-        self.driver.get("https://www.instagram.com/accounts/login/")
+        self.driver.get(f"{self.base_url}/accounts/login/")
         WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.NAME, "username")))
         self.driver.find_element_by_name('username').send_keys(self.username)
         self.driver.find_element_by_name('password').send_keys(self.password)
         self.driver.find_element_by_xpath('/html/body/div[1]/section/main/div/article/div/div[1]/div/form/div[4]').click()
 
+    def nav_user(self, user):
+        self.driver.get(f'{self.base_url}/{user}/')
+
 
 if __name__ == "__main__":
     ig_bot = InstagramBot("mr_magpie2020", "f9ptENkcPYUznCD")
+    ig_bot.nav_user('realmadrid')
 
 
 
